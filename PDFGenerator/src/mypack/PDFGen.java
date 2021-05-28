a2e25c5752029940e90af580a03a30020b0fd870
cryptidgit <ryannoliverr@gmail.com>
2021-05-28 16:14:41 -0400

    feat: added "no file selected" screen on application start

--- PDFGenerator/src/mypack/PDFGen.java
+++ PDFGenerator/src/mypack/PDFGen.java
@@ -18,11 +18,14 @@ class PDFGen {
 	
 	// Generate Application Window
 	private static void createWindow() {
+		JFrame nofile = new JFrame("PDF Generator"); // Default screen on opening
 		JFrame frame = new JFrame("PDF Generator");
 		
 		// Set up window frame and behavior
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500);
+		nofile.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		nofile.setSize(500, 500);
 		
 		// Create menu bar and items
 		JMenuBar bar = new JMenuBar();
@@ -36,8 +39,15 @@ class PDFGen {
 		
 		bar.add(file);
 		frame.setJMenuBar(bar);
+		nofile.setJMenuBar(bar);
+		
+		// Create "No File Selected" text
+		JLabel noselect = new JLabel("No File Selected");
+		noselect.setHorizontalAlignment(JLabel.CENTER);
+		
+		nofile.add(noselect);
 		
 		// Make changes visible
-		frame.setVisible(true);
+		nofile.setVisible(true);
 	}
 }
