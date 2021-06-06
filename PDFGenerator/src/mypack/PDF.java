ec517be4d6c2b0ec84876ada957884182dd50cb6
cryptidgit <ryannoliverr@gmail.com>
2021-06-06 18:58:43 -0400

    chore: created PDF constructor

--- PDFGenerator/src/mypack/PDF.java
+++ PDFGenerator/src/mypack/PDF.java
@@ -1,9 +1,32 @@
 package mypack;
 
+import java.io.FileNotFoundException;
+
 import com.itextpdf.*;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.layout.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class PDF {
-	private String name, dest;
+	private String name, dest, textFile;
+	
+	private Document doc;
+	private PdfWriter writer;
+	private PdfDocument pdfDoc;
+	
+	
+	PDF(String name, String dest) throws FileNotFoundException {
+		setName(name);
+		setDest(dest);
+		
+		writer = new PdfWriter(dest);
+		pdfDoc = new PdfDocument(writer);
+		doc = new Document(pdfDoc);
+
+	}
 	
 	private void setName(String name) {
 		this.name = name + ".pdf";
